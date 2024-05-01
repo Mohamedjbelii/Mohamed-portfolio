9e2dee33234685361e2267a7229bcd33e263649e
Mohamedjbelii <mohamedjbeli26@gmail.com>
2024-05-01 17:10:46 +0100

    first commit

--- src/constants.js
+++ src/constants.js
@@ -1,23 +1,22 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
-    I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
-    Here is my <a href="https://github.com/jslegenddev" target="_blank">Github</a>!`,
+  pc: `This is my PC. I work mostly in DevOps these days.
+  I'm a DevOps Engineer with a passion for learning JavaScript.This portfolio showcases my journey as I explore new technologies and combine them with my expertise in DevOps.   Anyway regardless of the language, I just like programming.
+    `,
   "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
-  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
-  I also make game programming tutorials on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a>! (If you like the content)
-  You'll learn how I built this portfolio you're currently playing through!`,
+  "sofa-table": `That's my sofa, where I spend countless hours self-developing my skills and acknowledgments. It's my go-to spot for learning, coding, and expanding my knowledge in DevOps, JavaScript, and cloud technologies. The comfort of this sofa fuels my passion for continuous improvement in both my personal and professional endeavors . 
+  For a glimpse into my achievements in cloud technologies, visit my Creddly profile to see my certifications and skill badges : <a href="https://www.credly.com/users/mohamed-jbeli.cb4bde45" target="_blank">here</a> and <a href=" https://www.credly.com/users/mohamed-jbeli.74a4fdd7" target="_blank">here</a>!.`,
   tv: `That's my TV. I've been watching tech youtubers a lot recently like :
-   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
-  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@TechWiz0001" target="_blank">techwizard</a>,
+  <a href="https://www.youtube.com/@amigoscode" target="_blank">Amigoscode</a> and <a href="https://www.youtube.com/@TechWorldwithNana" target="_blank">TechWorld with Nana </a> (realy helpful !).`,
   bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-  resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
-  Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/Mohamedjbelii/Resume/blob/main/Mohamed_Jbeli.pdf" target="_blank">Check it out?</a>
+  Contact me at mohamed-jbeli@live.fr if you have any interesting job opportunities!`,
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
-  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
-  Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. 
+  Here is my <a href="https://github.com/Mohamedjbelii" target="_blank">Github</a>!.`,
   library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
   I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
-  exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
+  exit: `If you want to exit Mohamed's portfolio, just close the tab.`,
 };
